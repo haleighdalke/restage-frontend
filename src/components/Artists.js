ec0f2162f92c12f74701522cfdb60d6d23c482c7
haleighdalke <haleighdalke@gmail.com>
2020-09-02 16:09:38 -0400

    added animation to artist view page

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -3,12 +3,13 @@ import React from 'react'
 export default class Artists extends React.Component {
 
     generateArtists = () => {
-        console.log(this.props.artists)
         return this.props.artists.map(artist => {
             return (
-                <div className="hexagon" key={artist.id} id={artist.id}>
+                <div className="hexagon">
                     <div className="hexagon-inside">
                         <div className="hexagon-image" style={{backgroundImage: `url(${artist.photo})`}}>
+                            <h3 className="artist-text">{artist.company_title}</h3>
+                            <p>{artist.description}</p>
                         </div>
                     </div>
                 </div>
