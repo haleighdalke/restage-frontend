073571b9a5386c36ba6a19144ee5beae382e9fb5
haleighdalke <haleighdalke@gmail.com>
2020-09-08 18:29:58 -0400

    filtering festivals in view artist component

--- src/components/ViewArtist.js
+++ src/components/ViewArtist.js
@@ -2,16 +2,53 @@ import React from 'react'
 
 export default class ViewArtist extends React.Component {
 
+    getReleaseDate = (release_date) => {
+        let dateArr = release_date.split("-").map(day => parseInt(day))
+        let currentDate = new Date(dateArr[0], dateArr[1], dateArr[2]) 
+        return currentDate.toDateString()
+    }
+
     generateFestivals = () => {
+
+        // commented out logic for separating upcoming vs current festivals
+
+        // let today = new Date(2020, 2, 23)
         let artist = this.props.artist
-        let artistFestivals = this.props.pieces.map(piece => {
-            if(piece.artist.id == artist.id){
+        // let currentFestivals = []
+        // let upcomingFestivals = []
+        let filteredPieces = this.props.pieces.filter(piece => piece.artist.id === artist.id)
+
+        // TRYING TO FILTER OUT DUPLICATE FESTIVALS STILL WORKING THRU
+        let temp = []
+        let uniqueFestivals = filteredPieces.filter(piece => {
+            if(!temp.includes(piece.festival.id)){
+                temp.push(piece.festival.id)
                 return piece.festival
             }
         })
 
-        let sortedFestivals = artistFestivals.sort((a, b) => b.release_date - a.release_date)
-        debugger
+                // let dateArr = piece.festival.release_date.split("-").map(day => parseInt(day))
+                // let currentDate = new Date(dateArr[0], dateArr[1], dateArr[2])
+                // if(currentDate <= today){
+                //     currentFestivals.push(piece.festival)
+                // }else{
+                //     upcomingFestivals.push(piece.festival)
+                // }
+
+        
+
+        // console.log(currentFestivals)
+        // console.log(upcomingFestivals)
+
+        return uniqueFestivals.map(festival => {
+            return(
+            <tr key={festival.id}>
+                <td>
+                    "{festival.title}" coming {this.getReleaseDate(festival.release_date)}
+                </td>
+            </tr>
+            )     
+        })
     }
 
     generateArtist = () => {
@@ -24,11 +61,11 @@ export default class ViewArtist extends React.Component {
                     <div className="artist-info">
                         <h1>{artist.title}</h1>
                         <h3>{artist.bio}</h3>
+                        <h2>Festivals: </h2>
                         <table>
-                            {this.generateFestivals()}
-                            <tr>
-
-                            </tr>
+                            <tbody>
+                                {this.generateFestivals()}
+                            </tbody>
                         </table>
                     </div>
                 </div>
